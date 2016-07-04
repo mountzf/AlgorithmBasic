08bb4d3b97591afa601e376940fdc42d00478865
mountzf <zhufeng1900@foxmail.com>
2016-07-04 16:23:30 +0800

    Change the INSERTION_SORT operation into a function INSERTION_SORT.

--- INSERTION_SORT.cpp
+++ INSERTION_SORT.cpp
@@ -2,21 +2,26 @@
 #include<cstdio>
 using namespace std;
 
+void INSERTION_SORT(int *array, int size)
+{
+    int key;
+    for(int i=1;i<size;++i)
+    {
+	key=*(array+i);
+	int j=i-1;
+	while((j>=0)&&(*(array+j)>=key))
+	{
+	    *(array+j+1)=*(array+j);
+	    --j;
+	}
+	*(array+j+1)=key;
+    }
+}
+
 int main()
 {
     int Num[10]={8,12,3,4,1,23,11,9,24,31};
-    int key;
-    for(int i=1;i<10;++i)
-    {
-	key=Num[i];
-	int j=i-1;
-	while((j>=0)&&(Num[j]>key))
-	{
-	    Num[j+1]=Num[j];
-	    --j;
-	}
-	Num[j+1]=key;
-    }
+    INSERTION_SORT(Num,10);
     for (int i=0;i<10;++i)
     {
 	printf("%d\t",Num[i]);
